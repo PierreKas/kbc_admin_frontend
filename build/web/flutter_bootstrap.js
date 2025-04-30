9da576fd3e79be9849b6ac879f299dfb4d745334
pierrekas <chikukaspierre@gmail.com>
2025-04-30 14:05:09 +0200

    title

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4162713007"
+    serviceWorkerVersion: "649665703"
   }
 });
